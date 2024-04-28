26763ed9e3f920fb002fd1a0144a4317cee845c4
Nonglak07 <130300978+Nonglak07@users.noreply.github.com>
2024-04-28 14:28:39 +0700

    Revert "week5"
    This reverts commit 95e608f6c40ec5a2f0dade846e491699a48dfa6c.

---
+++ components/week3/Section2.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, Image, } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import MyIcon from "./MyIcon";
+
+export default function Section2() {
+    return (
+        <View style={{ flex: 1, padding: 10}}>
+            <View style={{ flexDirection: "row" }}>
+                <View style={{ flex: 1, aspectRatio: 7 / 2, borderRadius: 10, backgroundColor: '#DCDCDC',marginTop: -45, }}  >
+                    <View style={{ marginTop: 10, justifyContent: 'center', alignItems: 'center' }}>
+                        <Text style={{ fontSize: 20 }}>Hilton San Francisco</Text>
+
+                        <View style={{ flexDirection: "row" }}>
+                            <FontAwesome name="star" size={20} color="orange" />
+                            <FontAwesome name="star" size={20} color="orange" />
+                            <FontAwesome name="star" size={20} color="orange" />
+                            <FontAwesome name="star" size={20} color="orange" />
+                            <FontAwesome name="star-half" size={20} color="orange" />
+                        </View>
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 16, color: 'gray' }}>Facilities provided may range from a modest quality mattress in a small room to large suites</Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </View>
+
+    );
+}
